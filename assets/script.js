c976af302fd7884a36333cf7ccb75ce629a6cd3f
Trystero <valesofhar@gmail.com>
2023-02-01 18:38:14 +0000

    Added GIPIHY API call

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,12 @@
 var giphyAPIKey="71Fx9egDGUwycT4oDZ7Bak9l6ifEFDNW";
 var rottenAPIKey=" ";
+
+
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=71Fx9egDGUwycT4oDZ7Bak9l6ifEFDNW";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+    });

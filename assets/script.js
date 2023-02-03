84ca6b8a2afa9acbdfa413872130a8c861d1243f
MBZAIG <muzzab@amazon.co.uk>
2023-02-03 04:08:16 +0000

    Added functionality to search for comic from marvel api and bring up the images of 4 comics and also brings up 4 giphys

--- assets/script.js
+++ assets/script.js
@@ -12,23 +12,23 @@ var gifSubmit1 = document.getElementById("button1");
 //Marvel
 
 //Marvel API Call structure : http://gateway.marvel.com/v1/public/comics?ts=1&apikey=PublicAPI&hash=md5(ts+privateAPI+PublicAPI) (the hash value is the md5 digest of 1abcd1234)
-function marvelSearch(comicName){
-  var ts= new Date().getTime();
-  console.log("TimeStampe "+ts);
-  var md5Hash=ts+marvelAPIKey+marvelPAPIKey;
-  console.log("Original MD5 "+ md5Hash);
+function marvelSearch(comicName) {
+  var ts = new Date().getTime();
+  console.log("TimeStampe " + ts);
+  var md5Hash = ts + marvelAPIKey + marvelPAPIKey;
+  console.log("Original MD5 " + md5Hash);
   var strHash = md5(md5Hash);
   console.log('The MD5 hash of the tutsplus string is:' + strHash);
-  var marvelQuery="https://gateway.marvel.com:443/v1/public/comics?format=comic&noVariants=true&titleStartsWith="+comicName+"&limit=5&ts="+ts+"&apikey="+marvelPAPIKey+"&hash="+strHash;
+  var marvelQuery = "https://gateway.marvel.com:443/v1/public/comics?format=comic&noVariants=true&titleStartsWith=" + comicName + "&limit=4&ts=" + ts + "&apikey=" + marvelPAPIKey + "&hash=" + strHash;
   $.ajax({
-      url: marvelQuery,
-      method: "GET"
-    }).then(function(marvelResponse) {
-      console.log(marvelResponse);
-      displayMarvel(marvelResponse);
-    });
-  }
-  // marvelSearch();
+    url: marvelQuery,
+    method: "GET"
+  }).then(function (marvelResponse) {
+    console.log(marvelResponse);
+    displayMarvel(marvelResponse);
+  });
+}
+// marvelSearch();
 
 // event listener for button click on form following user input
 // !variable just means if it's empty
@@ -36,18 +36,33 @@ comicSearch.addEventListener("click", validSearch);
 function validSearch(e) {
   if (!comicInput) {
     return;
-  }else {
+  } else {
     e.preventDefault();
-    var comicName = comicInput.value.replace(/ /g,"+");; // trim() removes only spaces, anything else to be removed, put in ""
+    var comicName = comicInput.value.replace(/ /g, "+");; // trim() removes only spaces, anything else to be removed, put in ""
     console.log(comicName);
     marvelSearch(comicName);
-    
-    
+    generateGIF(comicName);
+
   }
-  generateGIF(comicName);
+  
 }
 
+function displayMarvel(marvelResponse) 
+{
+  console.log("displayMarvelcAlled")
+  var marvelData = marvelResponse.data;
+  imageExtension = "." + marvelData.results[0].images[0].extension;
+   console.log(imageExtension);  
+  // console.log(marvelData.results[0].images[0].path+imageExtension);
+  for (var z = 0; z < marvelData.results.length; z++) {
+    console.log(z)
+    console.log("Marvel response=" + marvelData.results[z].images[0].path+imageExtension);
 
+   $('.marvelCard').append("<img src=" + marvelData.results[z].images[0].path+imageExtension +">")
+
+  }
+
+}
 
 // create function to pull data we want to display on HTML from the query
 
@@ -75,32 +90,31 @@ function validSearch(e) {
 // uses comicInput value to generate X GIFs based on the input from the search form
 // 
 // gifSubmit1.addEventListener("click", generateGIF);
-  
-  
- 
-function generateGIF(comicName){
 
-  var giphySearch= comicName+"+comic"
-  
-  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key="+giphyAPIKey+"&q="+giphySearch+"spider+man+comics&limit=4&offset=0&rating=g&lang=en" ;
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (giphyResponse) {
-  console.log(giphyResponse);
-  getGIF(giphyResponse);
-});
+
+
+function generateGIF(comicName) {
+
+  var giphySearch = comicName + "+comic"
+  console.log("comicNAMe "+"https://api.giphy.com/v1/gifs/search?api_key=" + giphyAPIKey + "&q=" + giphySearch + "&limit=4&offset=0&rating=g&lang=en")
+
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + giphyAPIKey + "&q=" + giphySearch + "&limit=4&offset=0&rating=g&lang=en";
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (giphyResponse) {
+    console.log(giphyResponse);
+    getGIF(giphyResponse);
+  });
 }
 
 // create function to pull data we want to display on HTML from the query
 function getGIF(giphyResponse) {
   var jiffy = giphyResponse.data;
-  console.log("Giphy Response data "+jiffy);
-  const gifLength = jiffy.length;
-  console.log("response lenght"+gifLength);
 
-  for (var i = 0; i < gifLength; i++) {
+  for (var i = 0; i < jiffy.length; i++) {
 
 
-    $('.giphyCard').append("<img src="+jiffy[i].images.original.url+"/>")
-}}
+    $('.giphyCard').append("<img src=" + jiffy[i].images.original.url + "/>")
+  }
+}
